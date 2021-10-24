2f11e430d27ef962c47ff4b8547dfaad5bbe6283
811misanthropy <scarlat.alexandru78@gmail.com>
2021-10-24 14:29:32 +0300

    commit3

---
+++ Seminar3/ScarlatS3JS.js
@@ -0,0 +1,12 @@
+function SumNum(a)
+{
+    let sum = 0;
+
+    for (let nrc = 1; nrc <= a; nrc++)
+    {
+    sum = sum + nrc;
+    }
+    console.log("Congratulations! You have successfully created an account");
+    console.log("Sum of all numbers up to", a, ":", sum);
+
+}
